b499f21192c19f8581ce05197293dec31cb5e5f1
Connect Nisa <116763724+Noorunnisa7@users.noreply.github.com>
2025-11-03 05:00:19 -0800

    variables and data type

---
+++ class1/index2.dart
@@ -0,0 +1,9 @@
+void main(){
+    //datatype variableName = value;
+    int number = 20;
+    int x = 34;
+    int y = 54;
+    print("Hello World");
+    print(number);
+    print(x+y);
+}
---
+++ class2/index.dart
@@ -0,0 +1,38 @@
+void main(){
+    print("Hello Dart Programming");
+    print("Hello World");
+    // variables and data types 
+    // datatype VariableName = value;
+    // int , String , double , bool , dynamic , final , const
+    int numbers = 123456;
+    String name = "Noor Un Nisa";
+    bool isRainging = false;
+    double temperature = 26.2;
+    String message = "Integer Data type: ";
+    int num1 = 1212;
+    num1 = 272;
+    num1 = 321;
+    name = "Connect Nisa";
+    var finalResult = "$message$num1";
+    print(numbers);
+    print("String: "+name);
+    print("Name: ${name}");
+    print(isRainging);
+    print(temperature);
+    print(finalResult);
+    int data = 56421;
+    // data = "Hello";
+    print(data);
+    const num2 = 50;
+    // num2 = 54;
+    print(num2);
+    dynamic userName = "Dart Data type";
+    userName = "Dart dynamic data type";
+    userName = 12356;
+    userName = true;
+    print(userName);
+
+    final country = 'Pakistan';
+    country = 'Iran';
+    print(country);
+}
